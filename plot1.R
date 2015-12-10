35beb6d83f7f9834448bf31402ae11d03669d5ad
iamnancy07 <iamnancy07@gmail.com>
2015-12-09 22:53:26 -0800

    add plot1.png and plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/kexin_000/Downloads/exdata-data-household_power_consumption")
+
+Dt <- read.table("household_power_consumption.txt", sep = ";", header = T, na.strings = "?", nrow = 2075259, stringsAsFactors = F )
+
+Dt1 <- subset(Dt, Date %in% c("1/2/2007", "2/2/2007"))
+
+
+Dt1$DateTime <- strptime(paste(Dt1$Date, Dt1$Time), format = "%d/%m/%Y %H:%M:%S")
+
+colnames(Dt1)
+
+hist(Dt1$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+
+dev.copy(png, file="C:\\Users\\kexin_000\\Exploratory-Data-Analysis\\plot1.png", height=480, width=480)
+dev.off()
